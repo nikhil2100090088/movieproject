e3e857330e5c37ceb45ec7eaa81021f1d0cf716a
nikhil2100090088 <2100090088csit@gmail.com>
2024-06-25 22:46:26 +0530

    Added

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,38 +1,44 @@
 import React, { useEffect, useState } from 'react'
 import MovieCard from './MovieCard'
 import axios from 'axios'
+import Pagination from './Pagination'
 
 const Movies = () => {
 
-    const [movies , setMovies] = useState( [])
+    const [movies, setMovies] = useState([])
+    const [pageNo, setPageNo] = useState(1)
 
+    const handlePrev = () => {
+        if (pageNo > 1) {
+            setPageNo(pageNo - 1)
+        }
+    }
 
-     useEffect(()=>{
-        axios.get('https://api.themoviedb.org/3/movie/popular?api_key=4cf5c7edf5a7a80e0b71d77f1a8b2a5d&language=en-US&page=1').then(function(res){ 
-             setMovies(res.data.results)
-             console.log(res.data.results)
-     })
-     } , [])
+    const handleNext = () => {
+        setPageNo(pageNo + 1)
+    }
 
+    useEffect(() => {
+        axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=4cf5c7edf5a7a80e0b71d77f1a8b2a5d&language=en-US&page=${pageNo}`)
+            .then(function (res) { 
+                setMovies(res.data.results)
+                console.log(res.data.results)
+            })
+    }, [pageNo])
 
-
-  return (
-    <div className='p-5'>
-        <div className='text-2xl m-5 font-bold text-center '>
-            Trending Movies
-
+    return (
+        <div className='p-5'>
+            <div className='text-2xl m-5 font-bold text-center '>
+                Trending Movies
+            </div>
+            <div className='flex flex-row flex-wrap justify-around gap-6'>
+                {movies.map((movieObj) => {
+                    return <MovieCard key={movieObj.id} poster_path={movieObj.poster_path} name={movieObj.original_title} />
+                })}
+            </div>
+            <Pagination pageNo={pageNo} handleNext={handleNext} handlePrev={handlePrev} />
         </div>
-        <div className='flex flex-row flex-wrap justify-around gap-6'>
-            
-            {movies.map((movieObj)=>{
-                return <MovieCard poster_path={movieObj.poster_path} name={movieObj.original_title}  />
-            })}
-            
-
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Movies
-// https://api.themoviedb.org/3/movie/popular?api_key=4cf5c7edf5a7a80e0b71d77f1a8b2a5d&language=en-US&page=1
--- src/components/WatchList.jsx
+++ src/components/WatchList.jsx
@@ -2,10 +2,26 @@ import React from 'react'
 
 const WatchList = () => {
   return (
-    <div> 
-        <h1> 
-            this is my WatchList
-        </h1>
+    <div className='flex flex-col items-center my-4'>
+      <input 
+        type="text" 
+        placeholder='Search Movies' 
+        className='h-[3rem] w-[18rem] border border-gray-300 mb-4'
+      />
+      
+      <table className='border-collapse border border-gray-200 w-full text-center'>
+        <thead className='border-b-2'>
+          <tr>
+            <th className='px-4 py-2'>Name</th>
+            <th className='px-4 py-2'>Popularity</th>
+            <th className='px-4 py-2'>Ratings</th>
+            <th className='px-4 py-2'>Genre</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* Table rows will go here */}
+        </tbody>
+      </table>
     </div>
   )
 }
