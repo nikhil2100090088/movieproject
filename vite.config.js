ef1b7071e5e2909884a7740ebcd93250f0d05f88
nikhil2100090088 <2100090088csit@gmail.com>
2024-11-06 23:48:54 +0530

    change repository name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/movieproject /', // Replace <repository-name> with your actual GitHub repository name
+  base: '/movieproject/', // Replace <repository-name> with your actual GitHub repository name
 })
