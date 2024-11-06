38dbdbdcf2af1b7fdd9974de422dcb88200c628b
nikhil2100090088 <2100090088csit@gmail.com>
2024-11-06 23:40:33 +0530

    Add base path for GitHub Pages deployment in vite.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/movieproject /', // Replace <repository-name> with your actual GitHub repository name
-})
